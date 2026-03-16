66fffb700ab43934cfcb803d4d84732548b6c1e8
Gopinath <maddula.gopinath173@gmail.com>
2026-03-16 18:21:53 +0530

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import ScrollToTop from "./components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <>
       <ScrollToTop />
+      <Analytics />
       <Header />
       <Body />
       <Footer />
